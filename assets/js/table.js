bf964b6efb2e6f8c07ca7b13f1c9b6c877eff507
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-03 00:30:26 +0700

    update

--- assets/js/table.js
+++ assets/js/table.js
@@ -11,12 +11,32 @@ export const formTodolist = `
 <td data-label="Date">#DEADLINE#</td>
 <td class="is-actions-cell">
   <div class="buttons is-right">
-    <button class="button is-small is-primary" type="button">
-      <span class="icon"><i class="mdi mdi-eye"></i></span>
+    <button class="button is-small is-primary" type="button" href="edit.html?nilaiId=#IDEDIT#" data-presensi-id="#IDHAPUS#">
+      <span class="icon"><i class="mdi mdi-eye">Edit</i></span>
     </button>
-    <button class="button is-small is-danger jb-modal" data-target="sample-modal" type="button">
-      <span class="icon"><i class="mdi mdi-trash-can"></i></span>
+    <button class="button is-small is-danger jb-modal" data-target="sample-modal" type="button" id="del_button" onclick="deleteData('#DELETE#')">
+      <span class="icon"><i class="mdi mdi-trash-can">Hapus</i></span>
     </button>
   </div>
 </td>
+
+<div id="sample-modal" class="modal">
+<div class="modal-background jb-modal-close"></div>
+<div class="modal-card">
+  <header class="modal-card-head">
+    <p class="modal-card-title">Confirm action</p>
+    <button class="delete jb-modal-close" aria-label="close"></button>
+  </header>
+  <section class="modal-card-body">
+    <p>This will permanently delete <b>Some Object</b></p>
+    <p>This is sample modal</p>
+  </section>
+  <footer class="modal-card-foot">
+    <button class="button jb-modal-close">Cancel</button>
+    <button class="button is-danger jb-modal-close">Delete</button>
+  </footer>
+</div>
+<button class="modal-close is-large jb-modal-close" aria-label="close"></button>
+</div>
+</div>
 `;
