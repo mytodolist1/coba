a30d8d3ade91d69c1f937aff191f983010db029f
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-03 01:52:42 +0700

    update

--- assets/js/formatdate.js
+++ assets/js/formatdate.js
@@ -1,32 +1,49 @@
+// import { getValue } from "https://jscroot.github.io/element/croot.js";
+
+// function formatDate(date) {
+//     const day = date.getDate().toString().padStart(2, '0');
+//     const month = (date.getMonth() + 1).toString().padStart(2, '0');
+//     const year = date.getFullYear();
+  
+//     return `${day}/${month}/${year}`;
+// }
+
+// const deadlineInputValue = getValue("deadline");
+
+// const deadlineDate = new Date(deadlineInputValue);
+
+// export const formattedDeadline = formatDate(deadlineDate);
+
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 
 function formatDate(date) {
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const year = date.getFullYear();
-  
-    return `${day}/${month}/${year}`;
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
 }
 
+// Mendapatkan nilai dari elemen dengan ID "deadline"
 const deadlineInputValue = getValue("deadline");
 
-const deadlineDate = new Date(deadlineInputValue);
+console.log("Deadline Input Value:", deadlineInputValue);
 
-if (isNaN(deadlineDate.getTime())) {
-    Swal.fire({
-        icon: "error",
-        title: "Invalid Deadline",
-        text: "Masukkan deadline yang valid.",
-    });
+// Memastikan variabel formattedDeadline terdefinisi dengan benar
+let formattedDeadline;
+
+// Jika nilai deadlineInputValue bukan tanggal yang valid
+if (!isNaN(Date.parse(deadlineInputValue))) {
+  // Mengonversi nilai deadline menjadi objek Date
+  const deadlineDate = new Date(deadlineInputValue);
+
+  console.log("Deadline Date:", deadlineDate);
+
+  formattedDeadline = formatDate(deadlineDate);
 } else {
-    const today = new Date();
-    if (deadlineDate < today) {
-        Swal.fire({
-            icon: "error",
-            title: "Invalid Deadline",
-            text: "Deadline tidak boleh kurang dari hari ini.",
-        });
-    }
+  console.error("Invalid Deadline Input Value");
 }
 
-export const formattedDeadline = formatDate(deadlineDate);
+console.log("Formatted Deadline:", formattedDeadline);
+
+export { formattedDeadline };
