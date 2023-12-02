7648f8b118ad2950ff45ed880ab72b2bfeb45976
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-02 17:12:06 +0700

    update

--- assets/js/get.js
+++ assets/js/get.js
@@ -1,36 +1,44 @@
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { formTodolist } from "./table.js";
-import { get } from "./process.js";
+// import { get } from "./process.js";
 
-function showTodolist(results) {
-    if (Array.isArray(results)) {
-        results.forEach(showData);
-        console.log(results);
-    } else {
-        console.error("Data received is not an array:", results);
+async function getTodolist() {
+    const tokens = getCookie("token");
+
+    if (!tokens) {
+        alert("Token not found, redirecting to login.");
+        return;
+    }
+
+    const target_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-get";
+
+    const myHeaders = new Headers();
+    myHeaders.append("token", tokens);
+
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    try {
+        const response = await fetch(target_url, requestOptions);
+        const result = await response.json();
+
+        if (result === true) {
+            displayTodolist(result.data);
+        } else {
+            alert(result.message);
+        }
+    } catch (error) {
+        console.log('error', error);
     }
 }
 
-// const urlParams = new URLSearchParams(window.location.search);
-// const user = urlParams.get("user");
-
-let targer_url = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-getTodo";
-
-function showData(data) {
-
-    const token = getCookie("token");
-        if (!token) {
-            console.log("Token not found, redirecting to login.");
-            return false;
-        } 
-
-    let form = formTodolist
-        .replace("#TITLE#", data.title)
-        .replace("#DESCRIPTION#", data.description)
-        .replace("#DEADLINE#", data.deadline);
-
-    addInner("tableTodolist", form);
+function displayTodolist(data) {
+    const table = document.getElementById("tableTodolist");
+    addInner(table, formTodolist(data));
 }
 
-get(targer_url, showTodolist);
+window.onload = getTodolist;
