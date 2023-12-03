541c1fa895a1f679b91103ce4630eb0c0821013d
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-03 14:01:05 +0700

    update

--- assets/js/table.js
+++ assets/js/table.js
@@ -17,3 +17,21 @@ export const formTodolist = `
   </div>
 </td>
 `;
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Handle click event on the Edit button
+  document.querySelectorAll('.jb-modal-edit').forEach((button) => {
+    button.addEventListener('click', () => {
+      // Show the modal
+      const editModal = document.getElementById('edit-modal');
+      editModal.classList.add('is-active');
+
+      // Close the modal when the user clicks outside of it
+      editModal.addEventListener('click', (event) => {
+        if (event.target.classList.contains('jb-modal-close') || event.target === editModal) {
+          editModal.classList.remove('is-active');
+        }
+      });
+    });
+  });
+});
