5c03bf97c0168420f7f91cbf28a1af25f1a7cee0
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-04 02:02:28 +0700

    update

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,12 +1,27 @@
 export const formTodolist = `
 <td class="is-checkbox-cell">
-<label class="b-checkbox checkbox">
-  <input type="checkbox" value="false">
-  <span class="check"></span>
-</label>
+  <label id="checkbox" class="b-checkbox checkbox" onchange="toggleUpdateSection()">
+    <input type="checkbox" value="false">
+    <span class="check"></span>
+  </label>
 </td>
 <td></td>
 <td data-label="Title">#TITLE#</td>
 <td data-label="Description">#DESCRIPTION#</td>
 <td data-label="Date">#DEADLINE#</td>
 `;
+
+const toggleUpdateSection = () => {
+  const checkbox = document.getElementById('checkbox');
+  const updateSection = document.querySelector('.modal');
+
+  if (checkbox.checked) {
+    updateSection.style.display = 'block';
+  } else {
+    updateSection.style.display = 'none';
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.toggleUpdateSection = toggleUpdateSection;
+});
