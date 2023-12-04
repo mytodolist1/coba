09c64d305c3ac8f113722d23aa64576baa702231
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-04 14:40:22 +0700

    update

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -1,33 +1,38 @@
 export const isiData = (results) => {
   const dataTodo = [
-    { id: "title", path: "title" },
-    { id: "description", path: "description" },
-    { id: "deadline", path: "deadline" },
+    {id: "title", path: "data.0.title"},
+    {id: "description", path: "data.0.description"},
+    {id: "deadline", path: "data.0.deadline"},
   ];
 
   console.log("isiData - Input:", results);
 
-  dataTodo.forEach(({ id, path }) => {
+  dataTodo.forEach(({ id, path, index, property }) => {
     const inputElement = document.getElementById(id);
-
-    if (inputElement) {
-      console.log(`Updating element with ID ${id} with data from path ${path}`);
-      const value = getNestedValue(results, path);
-      console.log(`Setting value for ${id}:`, value);
-      inputElement.value = value || ""; // Set default value to an empty string if value is falsy
-    } else {
-      console.warn(`Element with ID ${id} not found in the document.`);
-    }
+    console.log(`Updating element with ID ${id} with data from path ${path}`);
+    const value = getNestedValue(results, path, index, property);
+    console.log(`Setting value for ${id}:`, value);
+    inputElement.value = value;
   });
 }
 
-const getNestedValue = (obj, path) => {
-  console.log("getNestedValue - Input:", obj, path);
+const getNestedValue = (obj, path, index, property) => {
+  console.log("getNestedValue - Input:", obj, path, index, property);
   const value = path
     .split(".")
     .reduce((value, key) => (value && value[key] !== undefined ? value[key] : ""), obj);
 
   console.log("getNestedValue - Intermediate Value:", value);
 
+  if (
+    Array.isArray(value) &&
+    value.length > index &&
+    value[index].hasOwnProperty(property)
+  ) {
+    return value[index][property];
+  }
+
   return value;
 };
+
+  
