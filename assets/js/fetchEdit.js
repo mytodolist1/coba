5a3d294abc4e3b349aec20a18dd667a9488f8b72
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-04 13:02:16 +0700

    update

--- assets/js/fetchEdit.js
+++ assets/js/fetchEdit.js
@@ -2,6 +2,7 @@ import { isiData } from "./edit.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 const urlParams = new URLSearchParams(window.location.search);
+console.log("urlParams:", urlParams);
 const todoID = urlParams.get("id");
 
 console.log("todoID:", todoID);
