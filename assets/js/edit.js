0b436095485fbf3b10af767546db319b444fcd45
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-04 13:50:51 +0700

    update

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -7,23 +7,32 @@ export const isiData = (results) => {
 
   console.log("isiData - Input:", results);
 
-  dataTodo.forEach(({ id, path }) => {
+  dataTodo.forEach(({ id, path, index, property }) => {
     const inputElement = document.getElementById(id);
     console.log(`Updating element with ID ${id} with data from path ${path}`);
-    const value = getNestedValue(results, path);
+    const value = getNestedValue(results, path, index, property);
     console.log(`Setting value for ${id}:`, value);
     inputElement.value = value;
   });
 }
 
-const getNestedValue = (obj, path) => {
-  console.log("getNestedValue - Input:", obj, path);
-    const value = path
-      .split(".")
-      .reduce((value, key) => (value && value[key] ? value[key] : ""), obj);
+const getNestedValue = (obj, path, index, property) => {
+  console.log("getNestedValue - Input:", obj, path, index, property);
+  const value = path
+    .split(".")
+    .reduce((value, key) => (value && value[key] !== undefined ? value[key] : ""), obj);
 
-      console.log("getNestedValue - Intermediate Value:", value);
-  
-    return value;
+  console.log("getNestedValue - Intermediate Value:", value);
+
+  if (
+    Array.isArray(value) &&
+    value.length > index &&
+    value[index].hasOwnProperty(property)
+  ) {
+    return value[index][property];
+  }
+
+  return value;
 };
+
   
