adda9905973c100611d8bb68a31ba15e18fdb551
dirgafebrian <dirgafebrian45@gmail.com>
2023-12-04 12:53:33 +0700

    update

--- assets/js/fetchEdit.js
+++ assets/js/fetchEdit.js
@@ -4,6 +4,8 @@ import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 const urlParams = new URLSearchParams(window.location.search);
 const todoID = urlParams.get("id");
 
+console.log("todoID:", todoID);
+
 const urlFetch = "https://asia-southeast2-mytodolist-402507.cloudfunctions.net/mytodolist-getTodoByID?_id=" + todoID;
 
 function getTodoByID(target_url, responseFunction) {
